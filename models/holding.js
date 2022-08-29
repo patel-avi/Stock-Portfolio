562ff617168e95dd232a77892675889c9bf39c06
Avi Patel <avipatel@Avis-MacBook-Air.local>
2022-08-29 16:15:42 -0400

    Update holding and portfolio models to use referencing

---
+++ models/holding.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const holdingSchema = new Schema({
+  symbol: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  quantity: {
+    type: Number,
+  },
+  avgCost: {
+    type: Number,
+  },
+  price: {
+    type: Number,
+  },
+  marketValue: {
+    type: Number,
+  },
+  unrealizedGL: {
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("Holding", holdingSchema);
