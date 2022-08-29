005a0f789128266b5ab48608f66e2eb6d17d63c4
Avi Patel <avipatel@Avis-MacBook-Air.local>
2022-08-29 12:35:09 -0400

    Update portfolio and user models

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  googleId: String,
-  name: String,
-});
+const userSchema = new Schema(
+  {
+    googleId: String,
+    name: String,
+    email: String,
+    portfolios: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
