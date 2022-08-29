85ba35ab89889413e4e31476b352206cb9860c05
Avi Patel <avipatel@Avis-MacBook-Air.local>
2022-08-29 16:17:11 -0400

    Add post / and get /:id routes

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -4,6 +4,9 @@ const portfoliosCtrl = require("../controllers/portfolios");
 
 /* GET users listing. */
 router.get("/", portfoliosCtrl.index);
+// router.get("/new", portfoliosCtrl.new);
+router.post("/", portfoliosCtrl.new);
+router.get("/:id", portfoliosCtrl.show);
 
 router.get("/chart", function (req, res, next) {
   res.render("portfolios/chart", { title: "Chart" });
