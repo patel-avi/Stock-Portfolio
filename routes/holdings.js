f088e1cac6ecb475fddc3455a3651f0cfb4a67d4
Avi Patel <avi.patel@hotmail.com>
2022-08-30 17:18:49 -0400

    Add routes for editing and updating holdings

--- routes/holdings.js
+++ routes/holdings.js
@@ -3,5 +3,7 @@ var router = express.Router();
 const holdingsCtrl = require("../controllers/holdings");
 
 router.post("/portfolios/:id/holdings", holdingsCtrl.create);
+router.get("/portfolios/:id/holdings/:id2/edit", holdingsCtrl.edit);
+router.put("/portfolios/:id/holdings/:id2", holdingsCtrl.update);
 
 module.exports = router;
