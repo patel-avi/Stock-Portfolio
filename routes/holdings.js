f7e02d58ee9ef24f7cfd740295db22e4284c0eae
Avi Patel <avipatel@Avis-MacBook-Air.local>
2022-08-30 19:11:24 -0400

    Add delete holding route

--- routes/holdings.js
+++ routes/holdings.js
@@ -5,5 +5,6 @@ const holdingsCtrl = require("../controllers/holdings");
 router.post("/portfolios/:id/holdings", holdingsCtrl.create);
 router.get("/portfolios/:id/holdings/:id2/edit", holdingsCtrl.edit);
 router.put("/portfolios/:id/holdings/:id2", holdingsCtrl.update);
+router.delete("/portfolios/:id/holdings/:id2", holdingsCtrl.delete);
 
 module.exports = router;
