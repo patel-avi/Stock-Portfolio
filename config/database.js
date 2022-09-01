52a91c127946e1d6cb0ad7ac7b7dab22afc23628
Avi Patel <avi.patel@hotmail.com>
2022-09-01 11:58:51 -0400

    Fix db url

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost", {
+mongoose.connect(process.env.DATABASE_URL, {
   dbName: "Stock-Portfolio",
   useNewUrlParser: true,
   useUnifiedTopology: true,
